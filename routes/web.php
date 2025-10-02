852e54b3ba29c0e1dacad3931909dd9595eb5860
lucy <lucy.reyesruiz1@gmail.com>
2025-10-02 10:40:48 -0500

    archivo vista de bienvenido

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/greeting', function () {
+    return 'Hola lucy';
+});
+
+Route::get('/bienvenidos', function () {
+    return view(view:'bienvenidos');
+});
